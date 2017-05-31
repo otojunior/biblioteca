877ac984854d491b55885eb9d61e0b1e92c20df1
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-05-31 10:52:22 -0300

    Inicio do teste de unidade para o LivroDao

---
+++ biblioteca-ejb/src/test/java/org/otojunior/biblioteca/dao/livro/LivroDaoTest.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package org.otojunior.biblioteca.dao.livro;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.otojunior.biblioteca.dao.DaoBaseTest;
+import org.otojunior.biblioteca.entidade.livro.Livro;
+import org.otojunior.biblioteca.entidade.mock.LivroFabrica;
+import org.powermock.reflect.Whitebox;
+
+/**
+ * @author 01456231650
+ *
+ */
+public class LivroDaoTest extends DaoBaseTest {
+	private LivroDao dao;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		dao = new LivroDao();
+		Whitebox.setInternalState(dao, EntityManager.class, getEntityManager());
+	}
+	
+	/**
+	 * Test method for {@link org.otojunior.biblioteca.dao.livro.LivroDao#pesquisarPorId(java.lang.Long)}.
+	 */
+	@Test
+	public void testPesquisarPorId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.biblioteca.dao.livro.LivroDao#pesquisarPorNome(java.lang.String)}.
+	 */
+	@Test
+	public void testPesquisarPorNome() {
+		/*
+		 * Inserção de 10 livros, sendo que 5 deles terá o nome de "teste"
+		 */
+		getEntityManager().getTransaction().begin();
+		for (int i = 0; i < 10; i++) {
+			Livro livro = LivroFabrica.criar();
+			if (i % 2 == 0) {
+				livro.setNome("teste");
+			}
+			dao.persistir(livro);
+		}
+		getEntityManager().getTransaction().commit();
+		
+		assertEquals(5, dao.pesquisarPorNome("teste").size());
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.biblioteca.dao.livro.LivroDao#pesquisarPorEditora(java.lang.String)}.
+	 */
+	@Test
+	public void testPesquisarPorEditora() {
+		assertEquals(Collections.emptyList(), dao.pesquisarPorEditora("teste"));
+	}
+
+}
